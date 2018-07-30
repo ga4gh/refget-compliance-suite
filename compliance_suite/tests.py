f0893b1cfbb3382d3daf1f0e88415b310b9c3f65
Somesh <somesh.08.96@gmail.com>
2018-07-30 18:52:17 +0530

    Added in code comments in tests.py

--- compliance_suite/tests.py
+++ compliance_suite/tests.py
@@ -3,7 +3,23 @@ from metadata_algorithms import *
 
 
 class Test():
+    '''
+    Test Case class. All the test cases are intances of this class.
+    
+    label - used in graph algorithms to label the test case graph
+    alogorithm - Strategy design pattern in used here. It is the underlying algorithm used in the test case
+    result - 0 indicates skipped, 1 indicates passed, -1 indicates failed and 2 is not yet ran
+    pass_text - text in the report when test case is passed
+    skip_text - text in the report when test case is skipped
+    fail_text - text in the report when test case is failed
+    parents - dependencies of the test case to run
+    children - test cases which have this test case as dependency
+    warning - if the result of this test case is warning for the server implementation
+    '''
     def __init__(self, algorithm):
+        '''
+        Initiates the Test Case object. Algorithm is a required field to initiate test case object
+        '''
         self.label = 0
         self.algorithm = algorithm
         self.result = 2
@@ -15,18 +31,34 @@ class Test():
         self.warning = False
 
     def __str__(self):
+        '''
+        String repr of the test case
+        '''
         return 'test_' + self.algorithm.__name__
 
     def set_pass_text(self, text):
+        '''
+        Setter for pass_text
+        '''
         self.pass_text = text
 
     def set_fail_text(self, text):
+        '''
+        Setter for fail_text
+        '''
         self.fail_text = text
 
     def set_skip_text(self, text):
+        '''
+        Setter for skip_text
+        '''
         self.skip_text = text
 
     def generate_skip_text(self):
+        '''
+        Skip text is generated if there is no skip text (the case when test is skipped when the parent test cases fail or skip)
+        To track down the root cause of this skip.
+        '''
         text = str(self) + ' is skipped because' + '\n'
         for test in self.parents:
             if test.result != 1:
@@ -34,28 +66,47 @@ class Test():
         return text
 
     def add_parent(self, parent_test_case):
+        '''
+        Adds a parent test case
+        '''
         self.parents.append(parent_test_case)
 
     def add_child(self, child_test_case):
+        '''
+        Adds a child test case
+        '''
         self.children.append(child_test_case)
         child_test_case.add_parent(self)
 
     def toskip(self):
+        '''
+        Checks if any of the parent test cases failed or skipped which causes this case to skip
+        '''
         for test in self.parents:
             if test.result != 1:
                 return True
         return False
 
     def run(self, test_runner):
+        '''
+        First checks if the parent test cases were successful then run the text.
+        '''
+        # Checking if to skip
         if self.toskip() is True:
+            # warning will be generated because the test case is skipped because of some parent failure
             self.warning = True
             self.result = 0
             return
+        # run the test if not skipped
         self.algorithm(self, test_runner)
+        # if it fails it'll generate a warning
         if self.result == -1:
             self.warning = True
 
     def toecho(self):
+        '''
+        Returns the text based on the result of the test case
+        '''
         if self.result == 1:
             return self.pass_text
         elif self.result == -1:
@@ -66,6 +117,9 @@ class Test():
 
 
 def initiate_tests():
+    '''
+    Initiates test case objects and generates a test case graph for execution
+    '''
 
     # Base test case
     test_base = Test(base_algorithm)
