00733ad121487a3aba7ae9c0aa15ca168b715c0e
hulksmasher <somesh.08.96@gmail.com>
2018-08-05 14:55:02 +0530

    Matrix generation done

--- compliance_suite/report_server.py
+++ compliance_suite/report_server.py
@@ -1,14 +1,29 @@
 import http.server
 import socketserver
 import os
+import socket
 
 WEB_DIR = os.path.join(os.path.dirname(__file__), 'web')
 
 
+def get_free_port():
+    ''' get_free_port function is used in conftest and the return of this
+    is a free port available in the system on which the mock server will be
+    run. This port will be passed to start_mock_server as a required parameter
+    from conftest.py
+    '''
+
+    s = socket.socket(socket.AF_INET, type=socket.SOCK_STREAM)
+    s.bind(('localhost', 0))
+    address, port = s.getsockname()
+    s.close()
+    return port
+
+
 def start_mock_server(port=7878):
     os.chdir(WEB_DIR)
-
+    port = get_free_port()
     Handler = http.server.SimpleHTTPRequestHandler
     httpd = socketserver.TCPServer(("", port), Handler)
-    print("serving at port", port)
+    print("serving at localhost:", port)
     httpd.serve_forever()
--- compliance_suite/web/index.js
+++ compliance_suite/web/index.js
@@ -2,8 +2,9 @@ function load() {
     $.getJSON("temp_result.json", function (data) {
         var text_report = "";
         var num_reports = data.length;
+        var num_tests = data[0]["test_results"].length
+        console.log(data)
         $.each(data, function (index, report) {
-            console.log(report)
             text_report += "<h3>Server: " + report['server'] + "</h3>"
             text_report += "<p>Total tests: " + report["total_tests"] + "</p>"
             text_report += "<p>Total tests passed: " + report["total_tests_passed"] + "</p>"
@@ -33,5 +34,42 @@ function load() {
         });
 
         $("#text").html(text_report)
+
+        var t_head = "<tr><th>Test Cases</th>"
+        for(i=0; i<num_reports; i++){
+            t_head += "<th>" + data[i].server + "</th>"
+        }
+        t_head += "</tr>"
+        $("#compliance_matrix").find("thead").html(t_head)
+
+        var t_body = ""
+        for(i=0; i<num_tests; i++){
+            t_body += "<tr><td>" + data[0]["test_results"][i]["name"] + "</td>"
+            for(j=0; j<num_reports; j++){
+                var test = data[j]["test_results"][i]
+
+                if (test["result"] == 1){
+                    t_body += "<td class='text-success'>PASSED</td>"
+                }
+                else if(test["result"] == 0 && test["warning"] == true){
+                    t_body += "<td class='text-warning'>SKIPPED | WARNING</td>"
+                }
+                else if(test["result"] == 0 && test["warning"] == false){
+                    t_body += "<td class='text-info'>SKIPPED</td>";
+                }
+                else{
+                    t_body += "<td class='text-danger'>FAILED | WARNING</td>"
+                }
+            }
+            t_body += "</tr>"
+            console.log(t_body)
+        }
+        $("#compliance_matrix").find("tbody").html(t_body)
+
+
+
     });
+
+
+
 }
