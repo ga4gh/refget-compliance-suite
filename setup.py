101cb70d4961a50ec475aea4dc2b1a7e54430391
Andrew Yates <andyyatz@gmail.com>
2018-10-01 09:49:33 +0100

    Missed two tests. Doh! Changed to 206

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ install_requires = ['requests', 'click']
 
 setuptools.setup(
     name="refget-compliance",
-    version="1.2.0",
+    version="1.2.1",
     author="Somesh Chaturvedi",
     author_email="somesh.08.96@gmail.com",
     description="A compliance utility reporting system for refget server implementations",
